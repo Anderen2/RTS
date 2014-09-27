d182351dee672f7dc3a24969cdaf865838640a5d
Anderen2 <anderen2@xinity.co>
2014-09-27 22:38:10 +0200

    Fixed error in previous commit related to 64bit support

--- engine/Render/renderio.py
+++ engine/Render/renderio.py
@@ -44,10 +44,12 @@ class Input(FrameListener, OIS.MouseListener, OIS.KeyListener):
 	def SetupBare(self):
 		shared.DPrint("RenderIO",1,"Setting up OIS")
 		renderWindow = shared.renderRoot.getAutoCreatedWindow()
-		if architecture[0]=="64bit":
+
+		if architecture()[0]=="64bit":
 			windowHandle = renderWindow.getCustomAttributeUnsignedLong("WINDOW")
 		else:
 			windowHandle = renderWindow.getCustomAttributeInt("WINDOW")
+
 		paramList = [("WINDOW", str(windowHandle))]
 		self.inputManager = OIS.createPythonInputSystem(paramList)
 
@@ -69,7 +71,12 @@ class Input(FrameListener, OIS.MouseListener, OIS.KeyListener):
 		shared.DPrint("RenderIO",1,"Setting up OIS")
 		#The 4 lines below tells OIS what window to capture input from
 		renderWindow = shared.renderRoot.getAutoCreatedWindow()
-		windowHandle = renderWindow.getCustomAttributeUnsignedLong("WINDOW")
+
+		if architecture()[0]=="64bit":
+			windowHandle = renderWindow.getCustomAttributeUnsignedLong("WINDOW")
+		else:
+			windowHandle = renderWindow.getCustomAttributeInt("WINDOW")
+			
 		paramList = [("WINDOW", str(windowHandle))]
 		self.inputManager = OIS.createPythonInputSystem(paramList)
 
